0abfe14339b24263f232e52de0d69fbeb50d50df
littlecm <clittle.norml@gmail.com>
2024-02-06 10:16:41 -0500

    Update blueprint.js

--- blueprint.js
+++ blueprint.js
@@ -1,87 +1,46 @@
 const blueprint = {
-  "name": "SoldLog",
-  "slug": "sold_log",
+  "name": "Leads",
+  "slug": "leads",
   "fields": [
     {
-      "key": "Dealer",
-      "label": "Store",
+      "key": "Customer",
+      "label": "Customer",
       "type": "string"
     },
     {
-      "key": "Inventory Type",
-      "label": "Vehicle 1 Status",
-      "type": "string"
-    },
-    {
-      "key": "Year",
-      "label": "Vehicle 1 Year",
-      "type": "number"
-    },
-    {
-      "key": "Make",
-      "label": "Vehicle 1 Make",
-      "type": "string"
-    },
-    {
-      "key": "Model",
-      "label": "Vehicle 1 Model",
-      "type": "string"
-    },
-    {
-      "key": "Sold Date",
-      "label": "Date Sold",
-      "type": "date"
-    },
-    {
-      "key": "Stock Number",
-      "label": "Vehicle 1 Stock Number",
-      "type": "string"
-    },
-    {
-      "key": "VIN",
-      "label": "Vehicle 1 VIN",
-      "type": "string"
-    },
-    {
-      "key": "First Name",
-      "label": "Customer First Name",
-      "type": "string"
-    },
-    {
-      "key": "Last Name",
-      "label": "Customer Last Name",
-      "type": "string"
-    },
-    {
-      "key": "Cell Phone",
-      "label": "Cell Phone",
-      "type": "string"
-    },
-    // "Middle Name", "Daytime Phone", "Evening Phone", "Alt Email", "Buyer Birth Date", "Gender" fields can be added here if needed
-    {
-      "key": "email",
+      "key": "Customer Email",
       "label": "Customer Email",
       "type": "string"
     },
     {
-      "key": "City",
-      "label": "City",
+      "key": "Store",
+      "label": "Store",
       "type": "string"
     },
     {
-      "key": "State",
-      "label": "State",
+      "key": "Deal Date Created",
+      "label": "Deal Date Created",
+      "type": "date"
+    },
+    {
+      "key": "Customer First Name",
+      "label": "Customer First Name",
       "type": "string"
     },
     {
-      "key": "Postal Code",
-      "label": "Zip",
+      "key": "Customer Last Name",
+      "label": "Customer Last Name",
       "type": "string"
     },
     {
-      "key": "Total Gross",
-      "label": "DMS Total Gross",
-      "type": "number"
+      "key": "Source Type",
+      "label": "Source Type",
+      "type": "string"
+    },
+    {
+      "key": "Source Description",
+      "label": "Source Description",
+      "type": "string"
     }
   ]
 }

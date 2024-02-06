24d6be86f2dfe59e541d445ba5000354e21a1224
littlecm <clittle.norml@gmail.com>
2024-02-06 10:16:23 -0500

    Add files via upload

---
+++ blueprint.js
@@ -0,0 +1,87 @@
+const blueprint = {
+  "name": "SoldLog",
+  "slug": "sold_log",
+  "fields": [
+    {
+      "key": "Dealer",
+      "label": "Store",
+      "type": "string"
+    },
+    {
+      "key": "Inventory Type",
+      "label": "Vehicle 1 Status",
+      "type": "string"
+    },
+    {
+      "key": "Year",
+      "label": "Vehicle 1 Year",
+      "type": "number"
+    },
+    {
+      "key": "Make",
+      "label": "Vehicle 1 Make",
+      "type": "string"
+    },
+    {
+      "key": "Model",
+      "label": "Vehicle 1 Model",
+      "type": "string"
+    },
+    {
+      "key": "Sold Date",
+      "label": "Date Sold",
+      "type": "date"
+    },
+    {
+      "key": "Stock Number",
+      "label": "Vehicle 1 Stock Number",
+      "type": "string"
+    },
+    {
+      "key": "VIN",
+      "label": "Vehicle 1 VIN",
+      "type": "string"
+    },
+    {
+      "key": "First Name",
+      "label": "Customer First Name",
+      "type": "string"
+    },
+    {
+      "key": "Last Name",
+      "label": "Customer Last Name",
+      "type": "string"
+    },
+    {
+      "key": "Cell Phone",
+      "label": "Cell Phone",
+      "type": "string"
+    },
+    // "Middle Name", "Daytime Phone", "Evening Phone", "Alt Email", "Buyer Birth Date", "Gender" fields can be added here if needed
+    {
+      "key": "email",
+      "label": "Customer Email",
+      "type": "string"
+    },
+    {
+      "key": "City",
+      "label": "City",
+      "type": "string"
+    },
+    {
+      "key": "State",
+      "label": "State",
+      "type": "string"
+    },
+    {
+      "key": "Postal Code",
+      "label": "Zip",
+      "type": "string"
+    },
+    {
+      "key": "Total Gross",
+      "label": "DMS Total Gross",
+      "type": "number"
+    }
+  ]
+}
